ed30a5f6ea56801ad4a8fba6ac00c58366bfef77
Tonnette Stanford <tonnettestanford@yahoo.com.au>
2019-12-17 20:30:36 +1100

    changed date format

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $(document).on("click", ".city-btn", alertCityName);
-    var ExistingCityArray = [];
+    var m = moment();
     $(".redCard").hide()
+    console.log(m)
 
     var userStorageArray = JSON.parse(localStorage.getItem("savedSearches")) || [];
     storageArray();
@@ -50,87 +51,90 @@ $(document).ready(function() {
         var newCity = $(this).attr("data-name");
         var newURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + newCity + "&units=metric" + "&APPID=8260f022448e3f07d6465f550bc77374";
         $.ajax({
-                url: newURL,
-                method: "GET",
-                dataType: "jsonp",
-                statusCode: {
-                    404: function() {
-                        alert("city not found");
-                        return false;
-                    }
+            url: newURL,
+            method: "GET",
+            dataType: "jsonp",
+            statusCode: {
+                404: function () {
+                    alert("city not found");
+                    return;
                 }
+            }
 
-            })
+        })
             // this is getting the UV index info"
 
-        .then(function(mydata) {
-            var newlat = mydata.city.coord.lat;
-            var newlong = mydata.city.coord.lon;
-var newCoordsURL;
+            .then(function (mydata) {
+                var newlat = mydata.city.coord.lat;
+                var newlong = mydata.city.coord.lon;
+                var newCoordsURL;
 
-            if (location.protocol === 'http:') {
-                newCoordsURL ="http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + newlat + "&lon=" + newlong + "&APPID=8260f022448e3f07d6465f550bc77374";
-                ;
-             } else {
-                newCoordsURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + newlat + "&lon=" + newlong + "&APPID=8260f022448e3f07d6465f550bc77374";
+                if (location.protocol === 'http:') {
+                    newCoordsURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + newlat + "&lon=" + newlong + "&APPID=8260f022448e3f07d6465f550bc77374";
+                    ;
+                } else {
+                    newCoordsURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + newlat + "&lon=" + newlong + "&APPID=8260f022448e3f07d6465f550bc77374";
 
-             }
+                }
 
 
-            // var newCoordsURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + newlat + "&lon=" + newlong + "&APPID=8260f022448e3f07d6465f550bc77374";
-            console.log(newCoordsURL);
+                // var newCoordsURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + newlat + "&lon=" + newlong + "&APPID=8260f022448e3f07d6465f550bc77374";
+                console.log(newCoordsURL);
 
-            $.ajax({
+                $.ajax({
                     url: newCoordsURL,
                     method: "GET",
 
                 })
-                //     // We store all of the retrieved data inside of an object called "uvData"
-                .then(function(newUVData) {
+                    //     // We store all of the retrieved data inside of an object called "uvData"
+                    .then(function (newUVData) {
 
 
-                    $(".redCard").text(newUVData[0].value);
+                        $(".redCard").text(newUVData[0].value);
 
 
-                })
+                    })
 
-            // Log the queryURL
+                // Log the queryURL
 
-            $(".card").show();
-            $(".redCard").show();
-            $(".uvindex").show();
-            $(".five-day").show();
-            
-            // Transfer content to HTML
-            $(".city").text(mydata.city.name);
-            $(".date").text('(' + mydata.list[0].dt_txt.slice(0, -9) + ')');
-            var topIconcode = mydata.list[0].weather[0].icon;
-            var topIconurl = "https://openweathermap.org/img/w/" + topIconcode + ".png";
-            $("<img>").attr("src", topIconurl);
-            $(".iconTop").attr("src", topIconurl);
+                $(".card").show();
+                $(".redCard").show();
+                $(".uvindex").show();
+                $(".five-day").show();
+                var datecreate = moment().format('D-M-Y');
 
-            $(".temp").text("Temperature: " + mydata.list[0].main.temp + "°C");
-            $(".humidity").text("Humidity: " + mydata.list[0].main.humidity + "%");
-            $(".wind").text("Wind Speed: " + mydata.list[0].wind.speed + "MPH");
+                // Transfer content to HTML
+                $(".city").text(mydata.city.name);
+                $(".date").text('(' + datecreate + ')');
+                var topIconcode = mydata.list[0].weather[0].icon;
+                var topIconurl = "https://openweathermap.org/img/w/" + topIconcode + ".png";
+                $("<img>").attr("src", topIconurl);
+                $(".iconTop").attr("src", topIconurl);
+
+                $(".temp").text("Temperature: " + mydata.list[0].main.temp + "°C");
+                $(".humidity").text("Humidity: " + mydata.list[0].main.humidity + "%");
+                $(".wind").text("Wind Speed: " + mydata.list[0].wind.speed + "MPH");
 
 
-            for (var i = 0; i < 6; i++) {
+                for (var i = 0; i < 6; i++) {
 
-                var iconcode = mydata.list[i * 8].weather[0].icon;
-                var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-                $("<img>").attr("src", iconurl);
-                $(".icon" + i).attr("src", iconurl);
-                $(".tempCard" + i).text("Temp: " + mydata.list[i * 8].main.temp + "°C");
-                $(".humidCard" + i).text("Humidity: " + mydata.list[i * 8].main.humidity + "%");
-                $(".dateCard" + i).text(mydata.list[i * 8].dt_txt.slice(0, -9));
+                    var iconcode = mydata.list[i * 8].weather[0].icon;
+                    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+                    $("<img>").attr("src", iconurl);
+                    $(".icon" + i).attr("src", iconurl);
+                    $(".tempCard" + i).text("Temp: " + mydata.list[i * 8].main.temp + "°C");
+                    $(".humidCard" + i).text("Humidity: " + mydata.list[i * 8].main.humidity + "%");
+                    var timeTest = mydata.list[i * 8].dt_txt.slice(0, -9);
+             
+                    $(".dateCard" + i).text(timeTest);
 
-            }
-        });
+                }
+            });
     };
 
 
 
-    $(".mag").on("click", function() {
+    $(".mag").on("click", function () {
         event.preventDefault();
         var userCity = $("#searchField").val();
         var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + userCity + "&units=metric" + "&APPID=8260f022448e3f07d6465f550bc77374";
@@ -156,21 +160,21 @@ var newCoordsURL;
         function myAjax() {
             // // Here we run our AJAX call to the OpenWeatherMap API
             $.ajax({
-                    url: queryURL,
-                    method: "GET",
-                    dataType: "jsonp",
-                    statusCode: {
-                        404: function() {
-                            alert("city not found");
-                            // $("#searchField").clear();
-                            return false;
+                url: queryURL,
+                method: "GET",
+                dataType: "jsonp",
+                statusCode: {
+                    404: function () {
+                        alert("city not found");
+                        // $("#searchField").clear();
+                        return false;
 
-                        }
                     }
+                }
 
-                })
+            })
                 //     // this is getting the UV index info"
-                .then(function(data) {
+                .then(function (data) {
                     var lat = data.city.coord.lat;
                     var long = data.city.coord.lon;
 
@@ -179,25 +183,25 @@ var newCoordsURL;
                     var coordsURL;
 
                     if (location.protocol === 'http:') {
-                        coordsURL ="http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + long + "&APPID=8260f022448e3f07d6465f550bc77374";
+                        coordsURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + long + "&APPID=8260f022448e3f07d6465f550bc77374";
                         ;
-                     } else {
-                        coordsURL ="https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + long + "&APPID=8260f022448e3f07d6465f550bc77374";
-        
-                     }
-                   
-                   
+                    } else {
+                        coordsURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + long + "&APPID=8260f022448e3f07d6465f550bc77374";
+
+                    }
+
+
                     console.log("i want to know what the lat is: " + lat);
                     console.log("i want to know what the long is: " + long);
                     console.log(coordsURL);
 
                     $.ajax({
-                            url: coordsURL,
-                            method: "GET",
+                        url: coordsURL,
+                        method: "GET",
 
-                        })
+                    })
                         //     // We store all of the retrieved data inside of an object called "uvData"
-                        .then(function(UVData) {
+                        .then(function (UVData) {
 
                             console.log(UVData);
                             $(".redCard").text(UVData[0].value);
@@ -226,7 +230,20 @@ var newCoordsURL;
 
                     // Transfer content to HTML
                     $(".city").text(data.city.name);
-                    $(".date").text('(' + data.list[0].dt_txt.slice(0, -9) + ')');
+                    var now = moment();
+                    var readableTime = now.format('MMMM Do YYYY');
+                    console.log("readable time " + readableTime);
+
+
+
+                    var newreadableTime = data.list[0].dt_txt;
+                    console.log("new time " + newreadableTime);
+                    var create = moment().format('D-M-Y');
+                    console.log("moment " + create);
+                    // var format = data.list[0].dt_txt.format('MM D YYYY');
+
+                    // console.log("new format " + format)
+                    $(".date").text('(' + create + ')');
                     var topIconcode = data.list[0].weather[0].icon;
                     var topIconurl = "http://openweathermap.org/img/w/" + topIconcode + ".png";
                     $("<img>").attr("src", topIconurl);
